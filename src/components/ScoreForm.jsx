3743d47f4096ecd98ced1a8dbd5af1ede3ae8e1b
Zac Linke <zlinke@theblaze.com>
2018-11-12 22:18:37 -0500

    Fix score form styles. Fix typo in filename.

--- src/components/ScoreForm.jsx
+++ src/components/ScoreForm.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// TODO add TinyMCE or equivalent for Description textarea
 // import TinyMCEInput from 'react-tinymce-input';
 
 const ScoreForm = ({ currentForm, scores, updateScore }) => (
   <div>
-    <h4>Title</h4>
-    <input
-      className="large-text"
-      type="text"
-      onChange={ event => updateScore({ id: currentForm.id, title: event.target.value, field: 'title' }) }
-      value={ scores[currentForm.id - 1].title }
-    />
-    <h4>Description</h4>
+    <div className="form-group">
+      <div className="form-row">
+        <h4>Score Title</h4>
+        <input
+          className="form-control"
+          type="text"
+          onChange={ event => updateScore({ id: currentForm.id, title: event.target.value, field: 'title' }) }
+          value={ scores[currentForm.id - 1].title }
+        />
+      </div>
+    </div>
+    <div className="form-group">
+      <div className="form-row">
+        <h4>Description</h4>
+        <textarea className="form-control" />
+      </div>
+    </div>
   </div>
 );
 
